cdae929dfaf48982ede6f71860ff4651bcae51ac
Naiem890 <solaiman.naiem890@gmail.com>
2022-04-05 16:39:54 +0600

    rechart added extra-2

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from "react";
 import {
   Area,
   AreaChart,
+  Bar,
+  BarChart,
   CartesianGrid,
+  Legend,
   Line,
   LineChart,
+  Pie,
+  PieChart,
   ResponsiveContainer,
   Tooltip,
   XAxis,
@@ -23,13 +28,13 @@ const Dashboard = () => {
 
   return (
     <div className="md:mx-28 mx-8 my-10 ">
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2  gap-y-16">
         <div>
           <h3 className="text-xl uppercase font-bold  text-center mb-8 text-indigo-500">
             Month Wise Sell
           </h3>
           <LineChart
-            width={400}
+            width={500}
             height={300}
             className="mx-auto"
             data={data}
@@ -40,13 +45,14 @@ const Dashboard = () => {
             <XAxis dataKey="month" />
             <YAxis />
             <Tooltip />
+            <Legend />
           </LineChart>
         </div>
         <div>
           <h3 className="text-xl uppercase font-bold  text-center mb-8 text-indigo-500">
             Investment vs revenue
           </h3>
-          <ResponsiveContainer width={400} height={300} className="mx-auto">
+          <ResponsiveContainer width={500} height={300} className="mx-auto">
             <AreaChart
               width={500}
               height={400}
@@ -62,6 +68,8 @@ const Dashboard = () => {
               <XAxis dataKey="month" />
               <YAxis />
               <Tooltip />
+              <Legend />
+
               <Area
                 type="monotone"
                 dataKey="investment"
@@ -77,6 +85,59 @@ const Dashboard = () => {
                 fill="#82ca9d"
               />
             </AreaChart>
+          </ResponsiveContainer>
+        </div>
+        <div>
+          <h3 className="text-xl uppercase font-bold  text-center mb-8 text-indigo-500">
+            Investment vs revenue
+          </h3>
+          <ResponsiveContainer width={500} height={300} className="mx-auto">
+            <BarChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+              <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div>
+          <h3 className="text-xl uppercase font-bold  text-center mb-8 text-indigo-500">
+            Investment vs revenue
+          </h3>
+          <ResponsiveContainer width={500} height={300} className="mx-auto">
+            <PieChart width={400} height={400}>
+              <Pie
+                data={data}
+                dataKey="investment"
+                cx="50%"
+                cy="50%"
+                outerRadius={60}
+                fill="#8884d8"
+              />
+              <Pie
+                data={data}
+                dataKey="revenue"
+                cx="50%"
+                cy="50%"
+                innerRadius={70}
+                outerRadius={90}
+                fill="#82ca9d"
+                label
+              />
+            </PieChart>
           </ResponsiveContainer>
         </div>
       </div>
